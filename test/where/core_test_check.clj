134737f9d0d32f5cde569be541c59b1093f55241
Bruno Bonacci <bruno.bonacci@gmail.com>
2016-09-29 22:07:58 +0100

    added individual tests

--- test/where/core_test_check.clj
+++ test/where/core_test_check.clj
@@ -8,6 +8,7 @@
             [clojure.test.check.properties :as prop]))
 
 
+
 (def case-sensitive-string-ops
   [:contains? :starts-with? :ends-with? :is? :is-not?])
 
@@ -16,8 +17,6 @@
 
 (def string-ops
   (concat case-sensitive-string-ops case-insensitive-string-ops))
-
-
 
 (def gen-strings-or-nil
   (gen/frequency [[50 gen/string]
@@ -34,24 +33,27 @@
 
 
 (def prop-all-string-ops-are-nil-safe
-  (prop/for-all [v1 gen-strings-or-nil
-                 op (gen/elements string-ops)
-                 v2 gen-strings-or-nil]
-                (try ((where identity op v2) v1)
-                     true
-                     (catch NullPointerException x
-                       false))))
+  (prop/for-all
+   [v1 gen-strings-or-nil
+    op (gen/elements string-ops)
+    v2 gen-strings-or-nil]
+
+   (fact "prop all string ops are nil safe"
+         ((where identity op v2) v1)
+         =not=> (throws NullPointerException))))
+
 
 
 (def prop-all-string-ops-are-nil-safe-special-in
-  (prop/for-all [v1 gen-strings-or-nil
-                 op (gen/elements [:in? :IN?])
-                 v2 (gen/frequency [[50 (gen/list gen-strings-or-nil)]
-                                     [50 (gen/return nil)]])]
-                (try ((where identity op v2) v1)
-                     true
-                     (catch NullPointerException x
-                       false))))
+  (prop/for-all
+   [v1 gen-strings-or-nil
+    op (gen/elements [:in? :IN?])
+    v2 (gen/frequency [[50 (gen/list gen-strings-or-nil)]
+                       [50 (gen/return nil)]])]
+
+   (fact "prop all string ops are nil safe special in"
+         ((where identity op v2) v1)
+         =not=> (throws NullPointerException))))
 
 
 
@@ -77,26 +79,36 @@
   (when v
     (.toLowerCase v)))
 
+
 (def case-op-map
   (zipmap case-insensitive-string-ops case-sensitive-string-ops))
 
+
 (def prop-all-string-operation-support-case-insensitiveness
-  (prop/for-all [v1 gen-strings-or-nil
-                 op (gen/elements case-insensitive-string-ops)
-                 v2 gen-strings-or-nil]
-                (=
-                 ((where identity op v2) v1)
-                 ((where identity (case-op-map op) (lower v2)) (lower v1)))))
+  (prop/for-all
+   [v1 gen-strings-or-nil
+    op (gen/elements case-insensitive-string-ops)
+    v2 gen-strings-or-nil]
+
+   (fact "prop all string operation support case insensitiveness"
+         ((where identity op v2) v1)
+         =>
+         ((where identity (case-op-map op) (lower v2)) (lower v1)))))
+
 
 
 (def prop-all-string-operation-support-case-insensitiveness-special-in
-  (prop/for-all [v1 gen-strings-or-nil
-                 op (gen/return :IN?)
-                 v2 (gen/frequency [[50 (gen/list gen-strings-or-nil)]
-                                    [50 (gen/return nil)]])]
-                (=
-                 ((where identity op v2) v1)
-                 ((where identity :in? (when v2 (map lower v2))) (lower v1)))))
+  (prop/for-all
+   [v1 gen-strings-or-nil
+    op (gen/return :IN?)
+    v2 (gen/frequency [[50 (gen/list gen-strings-or-nil)]
+                       [50 (gen/return nil)]])]
+
+   (fact "prop all string operation support case insensitiveness special in"
+         ((where identity op v2) v1)
+         =>
+         ((where identity :in? (when v2 (map lower v2))) (lower v1)))))
+
 
 
 (fact "All string operations must support case insenstiveness" :test-check
